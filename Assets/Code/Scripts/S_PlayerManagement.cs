751204fb37c2f84ea5b20a832128be3a38fb7c3e
Meritxell Algueró <merialguerom@gmail.com>
2026-08-21 20:13:03 +0200

    Updated S_PlayerManagement
    Now when they move left they change sprite direction

--- Assets/Code/Scripts/S_PlayerManagement.cs
+++ Assets/Code/Scripts/S_PlayerManagement.cs
@@ -4,16 +4,22 @@ public class S_PlayerManagement : MonoBehaviour
 {
     [SerializeField] private float speed = 5f;
     [SerializeField] private float jumpSpeed = 5f;
-   private Rigidbody2D rBody;
+    float horizontal;
+    private Rigidbody2D rBody;
    
    private void Awake()
    {
     rBody = GetComponent<Rigidbody2D>();
+  
    }
 
     private void Update()
     {
-        rBody.linearVelocity = new Vector2(Input.GetAxis("Horizontal") * speed, rBody.linearVelocity.y);
+        horizontal = Input.GetAxis("Horizontal");
+        rBody.linearVelocity = new Vector2(horizontal * speed, rBody.linearVelocity.y);
+
+        if (horizontal < 0){ transform.localScale = new Vector3(-Mathf.Abs(transform.localScale.x), transform.localScale.y, transform.localScale.z);}
+        else if (horizontal > 0) { transform.localScale = new Vector3(Mathf.Abs(transform.localScale.x), transform.localScale.y, transform.localScale.z);}
 
         if (Input.GetButtonDown("Jump") && Mathf.Abs(rBody.linearVelocity.y) < 0.001f)
         {
